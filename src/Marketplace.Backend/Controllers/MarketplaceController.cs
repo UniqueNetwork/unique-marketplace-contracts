3d21c9a92894a68cdfdada1febdd0ebbf2672cac
sotmorskiy <sotmorskiy@usetech.ru>
2021-01-20 16:46:05 +0700

    Empty WebApi project.

---
+++ src/Marketplace.Backend/Controllers/MarketplaceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Marketplace.Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MarketplaceController : ControllerBase
+    {
+        private readonly ILogger<MarketplaceController> _logger;
+
+        public MarketplaceController(ILogger<MarketplaceController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
