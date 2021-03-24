271f1b997d259c4c0ddea19e9c8661f8d5575792
Greg Zaitsev <greg.zaitsev@gmail.com>
2021-03-24 19:17:21 +0300

    Remove hardcoding

--- src/Marketplace.Backend/Util/Address.cs
+++ src/Marketplace.Backend/Util/Address.cs
@@ -7,7 +7,7 @@ namespace Marketplace.Backend.Base58
 		public static byte[] AddressToPublicKey(string address)
         {
             byte[] publicKey = new byte[32];
-            var addressBytes = Base58Encoding.Decode("5Fj7qQR7f9uMNXTgj6bBJDKbaHbEnVb7c3tb881kchbDd82V");
+            var addressBytes = Base58Encoding.Decode(address);
             if (addressBytes.Length != 35) throw new FormatException();
             for (int i=0; i<32; i++)
                 publicKey[i] = addressBytes[i+1];
