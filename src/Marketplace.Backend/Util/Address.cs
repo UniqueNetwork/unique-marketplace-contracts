28788da89e854e1fcb2a64571fcea12f355accf6
Greg Zaitsev <greg.zaitsev@gmail.com>
2021-03-24 18:56:50 +0300

    Fix base58 decoding

--- src/Marketplace.Backend/Util/Address.cs
+++ src/Marketplace.Backend/Util/Address.cs
@@ -10,9 +10,17 @@ namespace Marketplace.Backend.Base58
             var addressBytes = Base58Encoding.Decode("5Fj7qQR7f9uMNXTgj6bBJDKbaHbEnVb7c3tb881kchbDd82V");
             if (addressBytes.Length != 35) throw new FormatException();
             for (int i=0; i<32; i++)
-                publicKey[i] = addressBytes[i+2];
+                publicKey[i] = addressBytes[i+1];
             return publicKey;
         }
     }
 
 }
+
+// Substrate format
+// 5FZeTmbZQZsJcyEevjGVK1HHkcKfWBYxWpbgEffQ2M1SqAnP
+// 2a9ac7639e339e24172f4197afb38c8a9594db2f957ae8232538efc68000a3f61fd91c
+
+// Kusama format
+// G5G85wS2vtENd46hS5YCxeHuCbuJrN8yCSRdKwMW9DwZz81
+// 029ac7639e339e24172f4197afb38c8a9594db2f957ae8232538efc68000a3f61f12fa
