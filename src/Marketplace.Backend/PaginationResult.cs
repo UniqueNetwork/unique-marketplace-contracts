f430210820086bc6ee7a607f7e0667410e07679a
sotmorskiy <sotmorskiy@usetech.ru>
2021-03-25 11:34:07 +0700

    Fixed warning.

--- src/Marketplace.Backend/PaginationResult.cs
+++ src/Marketplace.Backend/PaginationResult.cs
@@ -4,7 +4,7 @@ namespace Marketplace.Backend
 {
     public class PaginationResult<T>
     {
-        public IList<T> Items { get; set; }
+        public IList<T> Items { get; set; } = null!;
         public int ItemsCount { get; set; }
         public int Page { get; set; }
         public int PageSize { get; set; }
