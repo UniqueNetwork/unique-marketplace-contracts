853eda8c882756acf1950c39d626e3436deb46ca
Greg Zaitsev <greg.zaitsev@gmail.com>
2021-06-22 15:42:07 +0300

    Unique escrow code cleanup

--- src/Marketplace.Escrow.JS.Unique/unique_vault.js
+++ src/Marketplace.Escrow.JS.Unique/unique_vault.js
@@ -17,21 +17,16 @@ const incomingQuoteTxTable = "QuoteIncomingTransaction";
 const offerTable = "Offer";
 const tradeTable = "Trade";
 const outgoingQuoteTxTable = "QuoteOutgoingTransaction";
-const outgoingTxTable = "NftOutgoingTransaction";
+// const outgoingTxTable = "NftOutgoingTransaction";
 const uniqueBlocksTable = "UniqueProcessedBlock";
 let adminAddress;
 
-const rtt = require("./runtime_types.json");
 const contractAbi = require("./market_metadata.json");
 
 const quoteId = 2; // KSM
 
 let bestBlockNumber = 0; // The highest block in chain (not final)
 let timer;
-
-const blackList = [ 7395, 1745, 8587, 573, 4732, 3248, 6986, 7202, 6079, 1732, 6494, 7553, 6840, 4541, 2102, 3503, 6560, 4269, 2659, 3912, 3470, 6290, 5811, 5209, 8322, 1813, 7771, 2578, 2661, 2983, 2119, 3310, 1547, 1740, 3187, 8194, 4651, 6188, 2167, 3487, 3106, 6070, 3446, 2407, 5870, 3745, 6389, 3246, 9385, 9680, 6457, 8462, 2350, 3927, 2269, 8485, 6198, 6787, 2047, 2197, 2379, 2466, 2558, 2682, 2759, 2979, 4232, 4273, 8187, 8190, 2935, 2673, 5228, 7683, 2075, 9845, 1645, 3198, 7490, 3192, 7907, 3167, 858, 239, 7613, 2790, 7043, 5536, 8277, 1134, 6378, 2416, 2373, 2240, 3952, 5017, 4999, 5986, 3159, 6155, 9329, 6445, 2117, 3935, 6091, 7841, 8725, 5194, 5744, 8120, 5930, 578, 6171, 6930, 2180, 6212, 5963, 7097, 8774, 5233, 7978, 2938, 2364, 1823, 1840, 8672, 5616, 737, 6122, 8769, 615, 9729, 3489, 427, 9883, 8678, 6579, 1776, 7061, 873, 5324, 2390, 6187, 9517, 2321, 3390, 3180, 6692, 2129, 9854, 1572, 7412, 3966, 1302, 1145, 1067, 3519, 7387, 8314, 648, 219, 2055, 825, 1195
-];
-
 
 let resolver = null;
 function delay(ms) {
@@ -387,23 +382,6 @@ async function scanNftBlock(api, admin, blockNum) {
 
         let data = args[3].toString();
         log(`data = ${data}`);
-
-        // Quote Deposit registration
-        // 0x5eb1cb1f02000000000000000080c6a47e8d03000000000000000000f2536430fd61850d86660508a278f2cd5f7258ea1c1cf9491c5d848327c98121
-
-        // Buy 3457 (0D81):
-        // Block hash:   0x3a88c2efd7d7131f43f7771c3e50a98cd90cf9b1e8b83ed890207f98ea93495a
-        // Block number: 1,384,383
-        // Data:         0x261a70280400000000000000810d000000000000
-
-        // Withdraw NFT/Amount
-        // 0xb3f7898ecbda75ddf8f6cea3f584c9c46fb0fe7fbb645804c89261014b78ff22
-        // 1,384,398
-        // 0xe80efa690400000000000000860f0000…88e769c8628cc58d53fbe7f6fbc52b3e
-
-        // Register NFT 4744 (0x1288 = 8812 LE) deposit
-        // 0xe80efa6904000000000000008812000000000000d81f689c5d4aef49114017168ed953595ead394faa5acfd8877702693157620a
-
 
         // Ask call
         if (data.startsWith("0x020f741e")) {
