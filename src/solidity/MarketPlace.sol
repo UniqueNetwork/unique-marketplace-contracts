c5a9d74c851d86875e3eefaea18829e134da9242
stanta <st.takt@gmail.com>
2021-07-15 15:11:35 +0300

    started contracts

---
+++ src/solidity/MarketPlace.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier:  Apache License
+pragma solidity >= 0.8.0;
+
+contract MarketPlace {
+
+function ask (uint _price, uint  _currencyCode, uint _palet, uint _tokenID ) public payable { //
+
+
+}
+
+function buy (uint _palet, uint _tokenID ) public {
+
+}
+
+
+}
