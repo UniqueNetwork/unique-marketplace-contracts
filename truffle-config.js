3d304078b1fe4c29b954e89a63657339e0c6ebc9
stanta <st.takt@gmail.com>
2021-12-24 08:31:37 +0300

    gasPrice, polka2Eth addr and some fixings

--- truffle-config.js
+++ truffle-config.js
@@ -68,10 +68,10 @@ pm2 start  ganache-cli -- --m 'clutch captain shoe salt awake harvest setup prim
     upt: { // Unique Public testnet
 
         provider: () => new HDWalletProvider({
-            privateKeys: ["b8c1b5c1d81f9475fdf2e334517d29f733bdfa40682207571b12fc1142cbf329"],  
+            privateKeys: ["0xb8c1b5c1d81f9475fdf2e334517d29f733bdfa40682207571b12fc1142cbf329"],  
             providerOrUrl: "https://rpc-opal.unique.network/"}),/* http://35.157.131.180:9973/  http://15.236.177.137:9833/*/
             network_id: 8888,
-            gasPrice: '0x01'
+           // gasPrice: '0xEEA39D5A99'
             /** Eth: 0xf1a477099Ef8aA0f096be09A4CBBA858da993c41  
              * pk eth 3f13f692c4b88b5a6a317e6583623443658e37f9b7cbcbc54267b7b4d1c3f54c
              *             * 
